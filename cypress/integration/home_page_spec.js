c01c03fe0d64b5aaa386707decb9fb10544be826
Patrick Zimmerer <patrick.zimmerer1@gmail.com>
2022-03-28 15:09:14 +0200

    added cypress and started e2e testing

---
+++ cypress/integration/home_page_spec.js
@@ -0,0 +1,15 @@
+describe("The Home Page", () => {
+  it("successfully loads", () => {
+    cy.visit("http://localhost:63750"); // change URL to match your dev URL
+  });
+});
+describe("The Home Page", () => {
+  it("successfully loads", () => {
+    cy.visit("http://localhost:63750/user"); // change URL to match your dev URL
+  });
+});
+describe("The Home Page", () => {
+  it("successfully loads", () => {
+    cy.visit("http://localhost:63750/user/:id"); // change URL to match your dev URL
+  });
+});
